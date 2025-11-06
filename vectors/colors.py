9fe5dbdc7172f8de75c8746fe45b748e06e3e409
BtwX28 <100072844+BtwX28@users.noreply.github.com>
2025-11-06 13:05:51 +0100

    Create colors.py

---
+++ vectors/colors.py
@@ -0,0 +1,10 @@
+# colors.py
+# Contains the colors dictionary for the semantic space visualization
+
+def get_colors():
+    colors = {
+        "Elektronik": "green",
+        "Technik": "blue",
+        "Musik": "purple"
+    }
+    return colors
